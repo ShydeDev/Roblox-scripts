cb69f1169225aac0a950bc4b0ec329018c112db8
Shyde <156099484+ShydeDev@users.noreply.github.com>
2024-04-06 23:09:49 +0200

    Update infinite energy.lua

--- Blox Fruits/infinite energy.lua
+++ Blox Fruits/infinite energy.lua
@@ -2,6 +2,8 @@ local client = game:GetService("Players").LocalPlayer
 local energyPath = client.Character.Energy
 local maxEnergy = string.split(client.PlayerGui.Main.Energy.TextLabel.Text, "/")[2]
 
-energyPath.Changed:Connect(function()
-    energyPath.Value = maxEnergy
+energyPath:GetPropertyChangedSignal("Value"):Connect(function()
+    if energyPath.Value ~= maxEnergy then
+        energyPath.Value = maxEnergy
+    end
 end)
